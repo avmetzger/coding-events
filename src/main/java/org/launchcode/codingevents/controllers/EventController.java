723eaac6c41b9afb358777fdbea6fe7bbff1b812
Alex <avmetzger@gmail.com>
2020-02-12 19:08:42 -0600

    finished exercise in chapter 12

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -20,32 +20,47 @@ public class EventController {
         return "events/index";
     }
 
-    @GetMapping("create")
+    @GetMapping("/create")
     public String renderCreateEventForm(Model model) {
         model.addAttribute("title","Create Event");
         return"events/create";
     }
 
-    @PostMapping("create")
+    @PostMapping("/create")
     public String processCreateEventForm(@ModelAttribute Event newEvent) {
         EventData.add(newEvent);
         return "redirect:";
     }
 
-    @GetMapping("delete")
+    @GetMapping("/delete")
     public String renderDeleteEventsForm(Model model) {
         model.addAttribute("title","Delete Event");
         model.addAttribute("events",EventData.getAll());
         return "events/delete";
     }
 
-    @PostMapping("delete")
+    @PostMapping("/delete")
     public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds ){
         if (eventIds != null) {
             for (int id : eventIds){
                 EventData.remove(id);
             }
         }
-        return("redirect:");
+        return "redirect:";
+    }
+
+    @GetMapping("/edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        model.addAttribute("title","Edit Event " + EventData.getById(eventId).getName() + " (id = " + eventId + ")");
+        model.addAttribute("event", EventData.getById(eventId));
+        return "events/edit";
+    }
+
+    @PostMapping("/edit/{eventId}")
+    public String processEditForm(int eventId, String name, String description) {
+        Event event = EventData.getById(eventId);
+        event.setName(name);
+        event.setDescription(description);
+        return "redirect:redirect:";
     }
 }
